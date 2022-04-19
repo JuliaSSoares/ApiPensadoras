689151d0d5b017563039364e11958c7f7c30e665
Julia Soares <jusoaresbtu@gmail.com>
2022-04-19 19:54:26 -0300

    Criando classe de contexto

---
+++ ApiPensadoras/Context/ApiPensadorasDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiPensadoras.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPensadoras.Context
+{
+    public class ApiPensadorasDbContext : DbContext
+    {
+        public ApiPensadorasDbContext(DbContextOptions<ApiPensadorasDbContext> options ) : base( options )
+        {}
+
+        public DbSet<Pensadora>? Pensadoras { get; set; }
+        public DbSet<Frase>? Frases  { get; set; }    
+    }
+}
