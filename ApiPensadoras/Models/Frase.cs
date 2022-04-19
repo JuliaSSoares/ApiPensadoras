906a6ee99e918e46d1c49c706091d292112b5ad1
Julia Soares <jusoaresbtu@gmail.com>
2022-04-19 19:32:45 -0300

    Criando entidades do modelo de dominio

---
+++ ApiPensadoras/Models/Frase.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiPensadoras.Models;
+
+public class Frase
+{
+    public int FraseId { get; set; }
+    public string? ConteudoFrase { get; set; }
+    public DateTime? DataPublicacao { get; set; }
+}
+
---
+++ ApiPensadoras/Models/Pensadora.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiPensadoras.Models;
+
+public class Pensadora
+{
+    public int PensadoraId { get; set; }
+    public string? Nome { get; set; }
+    public string? Ocupacao { get; set; }
+    public string? Nacionalidade { get; set; } 
+    public DateTime DataDeNascimento { get; set; }
+    public DateTime? DataDeFalecimento { get; set; } 
+    public string? Descricao { get; set; }
+}
+
